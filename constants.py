f62c26bf063740c4463d81d87a6ba1aaacc0047c
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2021-09-27 16:10:33 -0400

    Added one predefined park

---
+++ constants.py
@@ -0,0 +1,3 @@
+GUEST_REP_CHAR = "🤮"
+STATE_WAITING = "Waiting"
+STATE_RIDING = "Riding"
---
+++ predefs.py
@@ -0,0 +1,12 @@
+from naming import get_rollercoaster_names
+from park_fixtures import Attraction
+
+
+def get_attractions_1():
+    names = get_rollercoaster_names(4)
+    attractions = [
+        Attraction(names[0], duration_ticks=2, capacity=20),
+        Attraction(names[1], duration_ticks=1, capacity=15),
+        Attraction("It's too small a world", duration_ticks=6, capacity=30),
+        Attraction(names[2], duration_ticks=1, capacity=30),
+    ]
